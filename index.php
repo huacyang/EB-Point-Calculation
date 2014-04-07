9a1804ed2ac7c65cd97c7e16d86208f3fde014eb
Hua Yang <huacyang@gmail.com>
2014-04-07 01:13:20 -0400

    added ability to read excel file & write csv file

--- index.php
+++ index.php
@@ -11,54 +11,73 @@
 
 require_once('Classes/PHPExcel.php');
 require_once('Classes/PHPExcel/IOFactory.php');
+require_once('Classes/PHPExcel/Writer/Excel2007.php');
 
-
+// the local json file name
 $jsonFile = "position";
+// the local excel file name
 $excelFile = "djFR2";
-
+// decodes the local json file 
 $string = file_get_contents($jsonFile.".json");
 $json = json_decode($string, true);
-
-//print_r($json[0]["name"]);
-
+// reads the local excel file
 $objReader = PHPExcel_IOFactory::createReader('Excel2007');
 $objPHPExcel = $objReader->load($excelFile.'.xlsx');
- 
-//Itrating through all the sheets in the excel workbook and storing the array data
+// iterates through all the sheets in the excel workbook and storing the array data
 foreach ($objPHPExcel->getWorksheetIterator() as $worksheet) {
     $arrayData[$worksheet->getTitle()] = $worksheet->toArray();
 }
 
-/*
- * Loops through the excel spreadsheet,
- *  grab all mouse click positions.
- */
 $dataField = $arrayData[$excelFile.'.txt'];
 $dataName = $dataField[0];
 $i_mouse = 0;
 $i_page = 0;
+$last_col = count($dataName);
+$dataField[0][$last_col] = "clicked_page";
 
+// searches for the column that has the mouse click position
 for (; $i_mouse < count($dataName); $i_mouse++)
 	if ($dataName[$i_mouse] == "MOUSE_CLICK_POSITION")
 		break;
-
+// searches for the column that has the current page number
 for (; $i_page < count($dataName); $i_page++)
 	if ($dataName[$i_page] == "CURRENT_PAGE")
 		break;
-
-echo "<pre>";
-print_r($json);
-echo "</pre>";
-
+// iterates through the excel file
 for ($i = 1; $i < count($dataField); $i++) {
-	$data = $dataField[$i][$i_mouse];
-	$page = $dataField[$i][$i_page];
+	$special = array("(", ")");
+	$mouse = $dataField[$i][$i_mouse];
+	$mouse = str_replace($special, "", $mouse);
+	$click = explode(",", $mouse);
+	$index = $dataField[$i][$i_page];
+	$page = $json[$index];
+	$position = $page['position'];
+	// iterates through the json file
+	for ($n = 0; $n < count($position); $n++) {
+		$token = explode(",", $position[$n]);
+
+		if ($click[0] > $token[0] &&
+			$click[0] < $token[2] &&
+			$click[1] > $token[1] &&
+			$click[1] < $token[3]) {
+
+			$dataField[$i][$last_col] = $page['link'][$n];
+			break;
+		}
+	}
+}
 
 echo "<pre>";
-	print_r($data);
-	print_r($page);
+print_r($dataField);
 echo "</pre>";
+
+$fp = fopen($excelFile.'.csv', 'w');
+
+foreach ($dataField as $field) {
+	fputcsv($fp, $field);
 }
+
+fclose($fp);
 
 ?>
 
